fc6f6288abb1edf3cb8d675c897dd87c114641b6
Holger Pirk <hlgr@dyn1206-82.wlan.ic.ac.uk>
2017-11-30 12:13:05 +0000

    added the extra (hidden) tests

--- ADBCoursework1.cpp
+++ ADBCoursework1.cpp
@@ -111,58 +111,136 @@ bool operator==(StarCount const& left, StarCount const& right) {
 };
 
 int main(int argc, char** argv) {
-
 	using namespace std;
-	database db("SA", "AdvancedDB17", "yelp", "146.169.45.80");
+	stringstream outstream;
+
+	outstream << "{";
+	if(argc > 1)
+		(outstream << "\"student\": \"" << argv[1] << "\", ").flush();
+
+	database db("SA", "AdvancedDB17", "yelp", "localhost");
 
 	{ // testing find Hours
 		auto hours = findHours(db, "kn");
 		std::sort(hours.begin(), hours.end());
-		cout << "findHours Test 1: " << (hours == hoursFixtureKn ? "passed" : "failed") << endl;
-
+		outstream << "findHoursTest1: " << (hours == hoursFixtureKn ? "'passed', " : "'failed', ");
 		hours = findHours(db, "neu");
 		std::sort(hours.begin(), hours.end());
-		cout << "findHours Test 2: " << (hours == hoursFixtureNeu ? "passed" : "failed") << endl;
-
+		outstream << "findHoursTest2: " << (hours == hoursFixtureNeu ? "'passed', " : "'failed', ");
 		hours = findHours(db, "Ewr");
 		std::sort(hours.begin(), hours.end());
-		cout << "findHours Test 3: " << (hours == hoursFixtureEwr ? "passed" : "failed") << endl;
+		outstream << "findHoursTest3: " << (hours == hoursFixtureEwr ? "'passed', " : "'failed', ");
+		hours = findHours(db, "Lucus");
+		std::sort(hours.begin(), hours.end());
+		outstream << "findHoursTest4: "
+							<< (hours == vector<string>{"Friday|10:00-16:00",		"Friday|10:00-4:00",
+																					"Friday|11:00-22:00",		"Monday|10:00-17:00",
+																					"Monday|10:00-2:00",		"Monday|11:00-22:00",
+																					"Saturday|10:00-17:00", "Saturday|10:00-4:00",
+																					"Saturday|16:00-22:00", "Sunday|10:00-2:00",
+																					"Sunday|16:00-22:00",		"Thursday|10:00-17:00",
+																					"Thursday|10:00-2:00",	"Thursday|11:00-22:00",
+																					"Tuesday|10:00-17:00",	"Tuesday|10:00-2:00",
+																					"Tuesday|11:00-22:00",	"Wednesday|10:00-17:00",
+																					"Wednesday|10:00-2:00", "Wednesday|11:00-22:00"}
+											? "'passed', "
+											: "'failed', ");
+		hours = findHours(db, "Razoun");
+		std::sort(hours.begin(), hours.end());
+		outstream
+				<< "findHoursTest5: "
+				<< (hours ==
+										vector<string>{
+												"Friday|10:00-19:00",		 "Friday|11:00-2:00",			"Friday|11:30-23:00",
+												"Friday|16:30-22:30",		 "Friday|17:30-22:30",		"Friday|5:30-23:00",
+												"Friday|6:00-2:00",			 "Friday|9:00-18:00",			"Friday|9:00-19:00",
+												"Monday|10:00-18:00",		 "Monday|11:00-2:00",			"Monday|11:30-22:00",
+												"Monday|16:30-21:00",		 "Monday|17:30-22:30",		"Monday|5:30-23:00",
+												"Monday|6:00-2:00",			 "Monday|9:00-19:00",			"Saturday|10:00-18:00",
+												"Saturday|11:00-2:00",	 "Saturday|11:30-23:00",	"Saturday|16:30-22:30",
+												"Saturday|17:30-22:30",	"Saturday|6:00-23:00",		"Saturday|6:00-2:00",
+												"Saturday|9:00-18:00",	 "Saturday|9:00-19:00",		"Sunday|11:00-2:00",
+												"Sunday|11:30-23:00",		 "Sunday|16:30-21:00",		"Sunday|6:00-2:00",
+												"Sunday|6:30-22:00",		 "Sunday|9:00-19:00",			"Thursday|10:00-19:00",
+												"Thursday|11:00-2:00",	 "Thursday|11:30-22:00",	"Thursday|16:30-21:00",
+												"Thursday|17:30-22:30",	"Thursday|5:30-23:00",		"Thursday|6:00-2:00",
+												"Thursday|9:00-18:00",	 "Thursday|9:00-19:00",		"Tuesday|10:00-18:00",
+												"Tuesday|11:00-2:00",		 "Tuesday|11:30-22:00",		"Tuesday|16:30-21:00",
+												"Tuesday|17:30-22:30",	 "Tuesday|5:30-23:00",		"Tuesday|6:00-2:00",
+												"Tuesday|9:00-18:00",		 "Tuesday|9:00-19:00",		"Wednesday|10:00-18:00",
+												"Wednesday|11:00-2:00",	"Wednesday|11:30-22:00", "Wednesday|16:30-21:00",
+												"Wednesday|17:30-22:30", "Wednesday|5:30-23:00",	"Wednesday|6:00-2:00",
+												"Wednesday|9:00-18:00",	"Wednesday|9:00-19:00"}
+								? "'passed', "
+								: "'failed', ");
+		hours = findHours(db, "Vyjay");
+		std::sort(hours.begin(), hours.end());
+		outstream << "findHoursTest6: "
+							<< (hours == vector<string>{"Friday|9:00-18:00", "Monday|9:00-18:00",
+																					"Saturday|9:00-15:30", "Thursday|9:00-18:00",
+																					"Tuesday|9:00-18:00", "Wednesday|9:00-18:00"}
+											? "'passed', "
+											: "'failed', ");
 	}
 
 	{ // testing countStars
 		auto stars = countStars(db, 30.0, 45.7, -100.0, -1.0);
 		std::sort(stars.begin(), stars.end(),
 							[](auto left, auto right) { return left.stars < right.stars; });
-		cout << "countStars Test 1: " << (stars == starFixture1 ? "passed" : "failed") << endl;
-
+		outstream << "countStarsTest1: " << (stars == starFixture1 ? "'passed', " : "'failed', ");
 		stars = countStars(db, 4.0, 40., -90.0, -40.0);
 		std::sort(stars.begin(), stars.end(),
 							[](auto left, auto right) { return left.stars < right.stars; });
-		cout << "countStars Test 2: " << (stars == starFixture2 ? "passed" : "failed") << endl;
+		outstream << "countStarsTest2: " << (stars == starFixture2 ? "'passed', " : "'failed', ");
+		stars = countStars(db, 41.99999, 44.00001, -80.000001, -74.99999);
+		std::sort(stars.begin(), stars.end(),
+							[](auto left, auto right) { return left.stars < right.stars; });
+		outstream << "countStarsTest3: "
+							<< (stars ==
+													std::vector<StarCount>{
+															{1, 49472}, {2, 44610}, {3, 77029}, {4, 134906}, {5, 123642}}
+											? "'passed', "
+											: "'failed', ");
+		stars = countStars(db, 32.99990, 34.00001, -112.49999, -111.50001);
+		std::sort(stars.begin(), stars.end(),
+							[](auto left, auto right) { return left.stars < right.stars; });
+		outstream << "countStarsTest4: "
+							<< (stars ==
+													std::vector<StarCount>{
+															{1, 167546}, {2, 86852}, {3, 104263}, {4, 224337}, {5, 526604}}
+											? "'passed'"
+											: "'failed'");
 	}
 
 	{ // performance runs
 
-		// warmup run
-		countStars(db, 30.0, 45.7, -100.0, -1.0);
-		for(size_t i = 0; i < 5; i++) {
+		try {
+			// warmup run
+			outstream << "\"TimesBeforeIndexing\": [";
 			countStars(db, 30.0, 45.7, -100.0, -1.0);
-			cout << "Run number " << i
-					 << " time before indexing: " << getLastQueryRuntime(db).elapsed_time << endl;
-		}
+			for(size_t i = 0; i < 5; i++) {
+				countStars(db, 30.0, 45.7, -100.0, -1.0);
+				outstream << getLastQueryRuntime(db).elapsed_time << (i < 4 ? ", " : "");
+			}
+			outstream << "], ";
 
-		createIndex(db);
+			createIndex(db);
 
-		// warmup run
-		countStars(db, 30.0, 45.7, -100.0, -1.0);
-		for(size_t i = 0; i < 5; i++) {
+			outstream << "\"TimesAfterIndexing\": [";
+			// warmup run
 			countStars(db, 30.0, 45.7, -100.0, -1.0);
-			cout << " Run number " << i
-					 << " time after indexing: " << getLastQueryRuntime(db).elapsed_time << endl;
+			for(size_t i = 0; i < 5; i++) {
+				countStars(db, 30.0, 45.7, -100.0, -1.0);
+				outstream << getLastQueryRuntime(db).elapsed_time << (i < 4 ? ", " : "");
+			}
+			outstream << "]";
+		} catch(exception& e) {
+			cerr << e.what() << endl;
 		}
-		cout << endl;
 
 		dropIndex(db);
 	}
+	outstream << "}" << endl;
+	cout << outstream.str();
 	return 0;
 }
